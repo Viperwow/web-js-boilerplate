da506d7cb99ec494baee15e9f28c42cb007c48e7
Viperwow <viperkodiak@gmail.com>
2018-12-25 15:32:03 +0300

    [gitlab-43] Add auto-import of stories of the `Storybook`

--- configs/storybook/config.js
+++ configs/storybook/config.js
@@ -1,6 +1,18 @@
 // Vendors
 const storybookReact = require('@storybook/react'); // eslint-disable-line import/no-extraneous-dependencies
-// Stories
-const stories = require('../../stories');
 
-storybookReact.configure(() => stories, module);
+const loadStories = () => {
+  /*
+    Automatically import all story js(x) files that end with *.stories.js
+    or *.stories.jsx with .flow extension or not
+  */
+  const req = require.context(
+    '../../src',
+    true,
+    /\.stories\.js(x)?(.flow)?$/,
+  );
+
+  req.keys().forEach(filename => req(filename));
+};
+
+storybookReact.configure(loadStories, module);
