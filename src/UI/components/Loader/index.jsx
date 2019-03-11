883cc40d691dcaa59032599a676e073741f690c0
Viperwow <viperkodiak@gmail.com>
2019-03-11 15:29:19 +0300

    [gitlab-59] Wipe out all `gemini` related things

---
+++ src/UI/components/Loader/index.jsx
@@ -0,0 +1,5 @@
+import {constant as _constant} from 'lodash';
+
+const Loader = _constant('Loading...');
+
+export default Loader;
--- src/common/connect.jsx
+++ src/common/connect.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import {ApolloConsumer} from 'react-apollo';
 import {merge} from 'lodash';
 
-const _doubleIdentity = (client, props = {}) => ({client, ...props});
+const _mixPropsWithClient = (client, props = {}) => ({client, ...props});
 
 const mergeProps = (
   client,
   props = {},
-  mapStateToProps = _doubleIdentity,
-  mapClientToProps = _doubleIdentity,
+  mapStateToProps = _mixPropsWithClient,
+  mapClientToProps = _mixPropsWithClient,
 ) => merge(
   Object.assign({}, mapStateToProps({store: client.extract().ROOT_QUERY}, props)),
   Object.assign({}, mapClientToProps(client, props)),
