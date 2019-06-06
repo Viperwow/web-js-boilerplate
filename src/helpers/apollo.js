f265d140ad2323aa37059c3c78a9b6c29038d1d3
Viperwow <viperkodiak@gmail.com>
2019-06-06 15:44:08 +0300

    [gitlab-70] Change GQL Apollo helper to be able to export function to get `ApolloClient` instance

--- src/helpers/apollo.js
+++ src/helpers/apollo.js
@@ -8,53 +8,57 @@ import {RetryLink} from 'apollo-link-retry';
 import {LOCALE_TYPE_DEF} from 'src/typedefs';
 import {setLocale} from 'src/resolvers';
 
-const cache = new InMemoryCache(); // Create apollo cache (same thing as meant as Redux app's 'local state')
+let _apolloClient = null;
 
-const httpLink = new HttpLink({
-  uri: 'https://launchpad.graphql.com/j90lv4pm5p', // 'http://localhost:3000/graphql',
-});
+export const initApolloClient = () => {
+  const cache = new InMemoryCache(); // Create apollo cache (same thing as meant as Redux app's 'local state')
 
-const localStateLink = withClientState({ // Allows you provide defaults to the 'local state'
-  cache,
-  defaults: {
-    locale: 'en',
-  },
-  typeDefs: `
+  const httpLink = new HttpLink({
+    uri: 'https://launchpad.graphql.com/j90lv4pm5p', // 'http://localhost:3000/graphql',
+  });
+
+  const localStateLink = withClientState({ // Allows you provide defaults to the 'local state'
+    cache,
+    defaults: {
+      locale: 'en',
+    },
+    typeDefs: `
     type Mutation {
       ${LOCALE_TYPE_DEF},
     }
   `,
-  resolvers: {
-    Mutation: {
-      setLocale,
+    resolvers: {
+      Mutation: {
+        setLocale,
+      },
     },
-  },
-});
+  });
 
-const errorLink = onError(({graphQLErrors, networkError}) => { // Just handles errors (log it, for example)
-  if (graphQLErrors) {
-    graphQLErrors.forEach(({message, locations, path}) => {
-      console.error(`[Apollo GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`); // eslint-disable-line max-len, no-console
-    });
-  }
+  const errorLink = onError(({graphQLErrors, networkError}) => { // Just handles errors (log it, for example)
+    if (graphQLErrors) {
+      graphQLErrors.forEach(({message, locations, path}) => {
+        console.error(`[Apollo GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`); // eslint-disable-line max-len, no-console
+      });
+    }
 
-  if (networkError) {
-    console.error(`[Apollo network error]: ${networkError}`); // eslint-disable-line no-console
-  }
-});
+    if (networkError) {
+      console.error(`[Apollo network error]: ${networkError}`); // eslint-disable-line no-console
+    }
+  });
 
-const retryLink = new RetryLink(); // To retry failed or time-outed http requests
+  const retryLink = new RetryLink(); // To retry failed or time-outed http requests
 
-const ApolloExtendedClient = new ApolloClient({
-  cache,
-  link: errorLink.concat( // Fourth: get an error if something totally went wrong
-    retryLink.concat( // Third: retry request if there were network error
-      ApolloLink.from([ // Mix links into a single link
-        localStateLink, // First: address to local cache and try to get data from here
-        httpLink, // Second: try to request something via GQL request
-      ]),
+  _apolloClient = new ApolloClient({
+    cache,
+    link: errorLink.concat( // Fourth: get an error if something totally went wrong
+      retryLink.concat( // Third: retry request if there were network error
+        ApolloLink.from([ // Mix links into a single link
+          localStateLink, // First: address to local cache and try to get data from here
+          httpLink, // Second: try to request something via GQL request
+        ]),
+      ),
     ),
-  ),
-});
+  });
+};
 
-export default ApolloExtendedClient;
+export const getApolloClient = () => _apolloClient;
