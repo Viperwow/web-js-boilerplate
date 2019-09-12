b1adb7d58e5eb0e79e74bdf9ff3f5ef747b4a11a
Viperwow <viperkodiak@gmail.com>
2019-09-12 13:40:55 +0300

    [gitlab-84] Add `postcss-flexbugs-fixes` to make flex usage smoother

--- configs/postcss.config.js
+++ configs/postcss.config.js
@@ -1,11 +1,13 @@
 const postcssPresetEnv = require('postcss-preset-env'); // eslint-disable-line import/no-extraneous-dependencies
 const cssnano = require('cssnano'); // eslint-disable-line import/no-extraneous-dependencies
+const postcssFlexbugsFixes = require('postcss-flexbugs-fixes'); // eslint-disable-line import/no-extraneous-dependencies
 
 module.exports = {
   plugins: [
     postcssPresetEnv({
       stage: 1,
     }),
+    postcssFlexbugsFixes({}),
     cssnano({
       preset: 'default',
       reduceIdents: {
