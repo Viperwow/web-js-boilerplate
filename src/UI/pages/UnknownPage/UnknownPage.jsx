896e595112e658acf5c6e104af440b2a222014e9
Viperwow <viperkodiak@gmail.com>
2019-09-12 11:54:58 +0300

    [gitlab-96] Go functional way with `eslint-plugin-react-hooks`
    - Add `eslint-plugin-react-hooks`
    - Convert all pages to functional components
    - Fix `unicorn/catch-error-nam`
    - Fix `.eslintrc` quotes to be single quotes
    - Fix `unfetch` package version

--- src/UI/pages/UnknownPage/UnknownPage.jsx
+++ src/UI/pages/UnknownPage/UnknownPage.jsx
@@ -1,33 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 
-class UnknownPage extends Component {
-  render() {
-    const {text} = this.props;
-
-    return (
-      <div className="page">
-        {text}
-        <ul>
-          <li>
-            <Link to="/1" replace>
-              1
-            </Link>
-          </li>
-          <li>
-            <Link to="/2" replace>
-              2
-            </Link>
-          </li>
-          <li>
-            <Link to="/3" replace>
-              3
-            </Link>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+const UnknownPage = ({text}) => (
+  <div className="page">
+    {text}
+    <ul>
+      <li>
+        <Link to="/1" replace>
+          1
+        </Link>
+      </li>
+      <li>
+        <Link to="/2" replace>
+          2
+        </Link>
+      </li>
+      <li>
+        <Link to="/3" replace>
+          3
+        </Link>
+      </li>
+    </ul>
+  </div>
+);
 
 export default UnknownPage;
