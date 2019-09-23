3b071f4ab60669a1d773320e018ca1efbaf82f1d
Viperwow <viperkodiak@gmail.com>
2019-09-23 13:53:42 +0300

    [gitlab-103] Add `Tailwind` css framework
    - Add `autoprefixer`, because `cssnano` doesn't actually add vendor prefixes

--- configs/postcss.config.js
+++ configs/postcss.config.js
@@ -1,13 +1,19 @@
 const postcssPresetEnv = require('postcss-preset-env'); // eslint-disable-line import/no-extraneous-dependencies
-const cssnano = require('cssnano'); // eslint-disable-line import/no-extraneous-dependencies
 const postcssFlexbugsFixes = require('postcss-flexbugs-fixes'); // eslint-disable-line import/no-extraneous-dependencies
+const postcssTailwind = require('tailwindcss'); // eslint-disable-line import/no-extraneous-dependencies
+const autoprefixer = require('autoprefixer'); // eslint-disable-line import/no-extraneous-dependencies
+const cssnano = require('cssnano'); // eslint-disable-line import/no-extraneous-dependencies
 
 module.exports = {
   plugins: [
     postcssPresetEnv({
       stage: 1,
     }),
-    postcssFlexbugsFixes({}),
+    postcssTailwind,
+    postcssFlexbugsFixes,
+    autoprefixer({
+      grid: 'autoplace',
+    }),
     cssnano({
       preset: 'default',
       reduceIdents: {
