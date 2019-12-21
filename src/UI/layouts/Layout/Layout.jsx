db4a41d5dcaf7ff7ba6ad93a28a2cdda10bd09cf
Viperwow <viperkodiak@gmail.com>
2019-12-21 23:32:12 +0300

    linaria test
    - Add more wrapped async steps to simplify whole module implementation switch
    - Fix build deps (caniuse-lite)

--- src/UI/layouts/Layout/Layout.jsx
+++ src/UI/layouts/Layout/Layout.jsx
@@ -8,20 +8,16 @@ import DynamicMainPage from 'src/UI/pages/MainPage';
 import DynamicSecondPage from 'src/UI/pages/SecondPage';
 import DynamicThirdPage from 'src/UI/pages/ThirdPage';
 import DynamicUnknownRoutePage from 'src/UI/pages/UnknownPage';
-import Button from 'src/UI/components/Button';
 
 const Layout = () => (
-  <div>
-    <Switch>
-      <Redirect exact from="/" to="/1" />
-      <Route exact path="/1" render={DynamicMainPage} />
-      <Route exact path="/2" render={DynamicSecondPage} />
-      <Route exact path="/3" render={DynamicThirdPage} />
-      <Route exact path="/button" render={() => <Button />} />
-      <Route exact path="/not-found" render={DynamicUnknownRoutePage} />
-      <Redirect to="/not-found" />
-    </Switch>
-  </div>
+  <Switch>
+    <Redirect exact from="/" to="/1" />
+    <Route exact path="/1" render={DynamicMainPage} />
+    <Route exact path="/2" render={DynamicSecondPage} />
+    <Route exact path="/3" render={DynamicThirdPage} />
+    <Route exact path="/not-found" render={DynamicUnknownRoutePage} />
+    <Redirect to="/not-found" />
+  </Switch>
 );
 
 export default Layout;
